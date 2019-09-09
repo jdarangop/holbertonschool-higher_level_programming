11bf54c4be189c19fbe4b2b02f5a02d29b2c298f
jdarangop <juandiegoap33@gmail.com>
2019-09-09 19:45:49 +0000

    Add 10 task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Check if a list is a cycle.
+ * @list: the list to check.
+ * Return: 1 if is a cycle, 0 if doesn't.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *advanced = list;
+	listint_t *retraced = list;
+
+	while (advanced != NULL && advanced->next != NULL)
+	{
+		advanced = advanced->next;
+		
+		if (advanced == retraced)
+			return (1);
+	}
+	return (0);
+}
