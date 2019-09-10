22789760e4d800a9ffb12b7f3f6ad9d56bb85a8c
jdarangop <juandiegoap33@gmail.com>
2019-09-10 17:33:50 +0000

    Fix 5 task and fix style in 6 task

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ number = random.randint(-10000, 10000)
 tmp = str(number)
 tmp = tmp[-1]
 tmp = int(tmp)
-if (number > 5):
+if (tmp > 5):
     print("Last digit of {} is {} and is greater than 5".format(number, tmp))
-elif (number == 0):
+elif (tmp == 0):
     print("Last digit of {} is {} and is 0".format(number, tmp))
-elif (number < 6 and number != 0):
+elif (tmp < 6 and tmp != 0):
     print("Last digit of {} is {} and \
 is less than 6 and not 0".format(number, tmp))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if (i != 9 or j != 9):
-            print("{}{}".format(i, j), end = ", ") 
-        else:
-            print("{}{}".format(i, j))
+for i in range(100):
+    if (i != 99):
+        print("{:d}{}".format(int(i/10), int(i%10)), end = ", ") 
+    else:
+        print("{:d}{}".format(int(i/10), int(i%10)))
