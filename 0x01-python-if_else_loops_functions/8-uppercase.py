533df1548fdd4feb6dc8e1014c390a69d43e8d7b
jdarangop <juandiegoap33@gmail.com>
2019-09-10 22:03:25 +0000

    Fix shebang in files

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         tmp = str[i]
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
             tmp = chr(ord(str[i]) - 32)
         print(tmp, end="")
     print()
