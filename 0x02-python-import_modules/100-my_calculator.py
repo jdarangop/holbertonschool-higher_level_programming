ff077c5fae22066d2a09a299c7e9a20b4aae3179
jdarangop <juandiegoap33@gmail.com>
2019-09-12 20:55:26 +0000

    Fix first advanced task and add second advanced task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,10 +16,8 @@ if __name__ == '__main__':
             if argv[2] == "+":
                 print("{} + {} = {}".format(a, b, add(a, b)))
             elif argv[2] == "-":
-                print("{} + {} = {}".format(a, b, sub(a, b)))
+                print("{} - {} = {}".format(a, b, sub(a, b)))
             elif argv[2] == "*":
-                print("{} + {} = {}".format(a, b, mul(a, b)))
+                print("{} * {} = {}".format(a, b, mul(a, b)))
             elif argv[2] == "/":
-                print("{} + {} = {}".format(a, b, div(a, b)))
-            exit(0)
-
+                print("{} / {} = {}".format(a, b, div(a, b)))
