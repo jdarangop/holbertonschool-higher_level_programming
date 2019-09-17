41a2de56d069936ceb302e0ffe42f812df5d8cba
jdarangop <juandiegoap33@gmail.com>
2019-09-17 23:15:47 +0000

    Fix 13 task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,33 +1,5 @@
 #include "lists.h"
 
-/**
- * palindromo - Auxiliar variable with recursivity.
- * @head: Head of the linked list.
- * @num: Counter for each cicle, it going decreassing.
- * @len: lenght to rebase if is a palindrome.
- * Return: Always 0.
- */
-int palindromo(listint_t *head, int *num, int len)
-{
-	listint_t *inicio, *current;
-	int i;
-
-	inicio = head;
-	current = head;
-	for (i = 1; i < *num; i++)
-	{
-		current = current->next;
-	}
-	if (*num < len)
-		return (1);
-	else if (inicio->n == current->n)
-	{
-		*num -= 2;
-		return (palindromo(head->next, num, len));
-	}
-	else
-		return (0);
-}
 
 /**
  * is_palindrome - Check if a linked list is a palindrome.
@@ -36,19 +8,36 @@ int palindromo(listint_t *head, int *num, int len)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int lenght = 0, num = 0;
+	listint_t *current = *head, *inicio, *final;
+	int lenght = 0, count = 0, i = 0, j;
 
 	while (current->next != NULL)
 	{
 		lenght++;
 		current = current->next;
 	}
+	count = lenght + 1;
+	while (i != count / 2)
+	{
+		inicio = *head;
+		final = *head;
 
-	num = lenght + 1;
-	if (num % 2 == 0)
-		lenght = 2;
-	else
-		lenght = (lenght - 1) / 2;
-	return (palindromo(*head, &num, lenght));
+		for (j = 0; j < i; j++)
+		{
+			inicio = inicio->next;
+		}
+		for (j = 0; j < count - (i + 1); j++)
+		{
+			final = final->next;
+		}
+		if (inicio->n != final->n)
+		{
+			return (0);
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (1);
 }
