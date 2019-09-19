e324f045e10402162fcdd19f9c741ec106382095
jdarangop <juandiegoap33@gmail.com>
2019-09-19 17:31:08 +0000

    Add 0x04-python-more_dat_structures folder

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    keys = list(a_dictionary.keys())
+    maximo = a_dictionary[keys[0]]
+    for i in a_dictionary.keys():
+        tmp = a_dictionary.get(i)
+        if tmp > maximo:
+            maximo = tmp
+            key = i
+    return key
