9abde2332aaa287269b2614c7c921156e1c2ed72
jdarangop <juandiegoap33@gmail.com>
2019-09-19 18:20:29 +0000

    Change the initial condition to maximum

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
     keys = list(a_dictionary.keys())
-    maximo = a_dictionary[keys[0]]
+    maximo = -100000000 
     for i in a_dictionary.keys():
         tmp = a_dictionary.get(i)
         if tmp is None:
