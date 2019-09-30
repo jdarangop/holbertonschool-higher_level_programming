2b7b930d55713ed5752b666dfea7e6c2bc097ecc
jdarangop <juandiegoap33@gmail.com>
2019-09-30 20:06:27 +0000

    Fix second advanced task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        fct(args[0], args[1])
+        result = fct(args[0], args[1])
     except Exception as e:
+        result = None
         sys.stderr.write('Exception: ' + str(e) + '\n')
+    return result
