9d64a80fa9f605e82c43877f8970f340f33994bb
jdarangop <juandiegoap33@gmail.com>
2019-10-01 21:13:21 +0000

    Fix 6 task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,8 +3,10 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         if type(size) != int:
             raise TypeError('size must be an integer')
+            return
         elif size < 0:
             raise ValueError('size must be >= 0')
+            return
         else:
             self.__size = size
 
