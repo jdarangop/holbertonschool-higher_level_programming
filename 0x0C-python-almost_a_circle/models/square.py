12b8c38db545055597201295cf8653dfa53498fe
jdarangop <juandiegoap33@gmail.com>
2019-10-22 02:42:57 +0000

    Add 0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Module Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Init method"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Width getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Width setter"""
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__size = value
+        self.width = value
+        self.heoght = value
+
+    def update(self, *args, **kwargs):
+        """Update method"""
+        attributes = ["id", "size", "x", "y"]
+        if args is not None:
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
+        if len(args) == 0 or args[0] == "":
+            if kwargs is not None:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """To dictionary method"""
+        attributes = ["id", "size", "x", "y"]
+        values = [self.id, self.size, self.x, self.y]
+        return dict(zip(attributes, values))
