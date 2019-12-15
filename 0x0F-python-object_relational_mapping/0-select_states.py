e692f576202993721c35755a2d76f7acba4ceec0
jdarangop <juandiegoap33@gmail.com>
2019-12-15 18:07:09 +0000

    Feature: add ASC to task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=db_name, charset="utf8")
     cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY states.id ASC;')
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     for i in cur.fetchall():
         print (i)
     cur.close()
