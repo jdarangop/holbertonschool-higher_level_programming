7ea8627e9de0fa5dbb0a1129bea82a4bc8866916
jdarangop <juandiegoap33@gmail.com>
2019-12-15 17:42:40 +0000

    Feature: add charset to task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=db_name)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name, charset="utf8")
     cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY states.id;')
     for i in cur.fetchall():
