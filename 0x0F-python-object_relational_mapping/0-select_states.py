b644cfc876db8e7b7e19a2a67ab22cb89cc855e4
jdarangop <juandiegoap33@gmail.com>
2019-12-15 09:07:58 +0000

    Try: fix requeriment

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password, db=db_name)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY states.id')
+    cur.execute('SELECT * FROM states ORDER BY states.id;')
     for i in cur.fetchall():
         print (i)
     cur.close()
