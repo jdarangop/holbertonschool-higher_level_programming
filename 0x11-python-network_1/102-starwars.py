d85dc2bef487ac13a74b98924001a2c25e2e2ebb
jdarangop <juandiegoap33@gmail.com>
2020-01-14 04:29:18 +0000

    Feature: add task 13 and Fix: task 12

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -4,6 +4,13 @@ import requests
 import sys
 
 
+def print_films(url):
+    """ Print the titles of the films"""
+    resultado = requests.get(url)
+    dict_url = resultado.json()
+    print("\t{}".format(dict_url.get("title")))
+
+
 if __name__ == "__main__":
 
     arg = sys.argv[1]
@@ -15,8 +22,12 @@ if __name__ == "__main__":
     print("Number of results: {}".format(dict_data.get("count")))
     for i in dict_data.get("results"):
         print(i.get('name'))
+        for j in i.get('films'):
+            print_films(j)
     if dict_data.get("next") is not None:
         result = requests.get(dict_data.get("next"), params=value)
         dict_data = result.json()
         for i in dict_data.get("results"):
             print(i.get('name'))
+            for j in i.get('films'):
+                print_films(j)
