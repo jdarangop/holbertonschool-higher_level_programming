66067b2cb6755f017d1125b86ef5ba669726cec9
jdarangop <juandiegoap33@gmail.com>
2020-01-14 16:43:21 +0000

    Fix: only display the 10 most recent commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,6 +12,11 @@ if __name__ == "__main__":
                           format(owner, repo))
 
     dict_json = result.json()
-    for i in dict_json:
+    if len(dict_json) > 10:
+        n = 10
+    else:
+        n = len(dict_json)
+    for j in range(n):
+        i = dict_json[j]
         print("{}: {}".format(i.get('sha'), i.get('commit').
               get('author').get('name')))
