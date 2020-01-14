fbc4f07dc06977d3037e3bb43b6f70d85c9e47de
jdarangop <juandiegoap33@gmail.com>
2020-01-14 02:52:42 +0000

    Feature: add task 11

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Star wars API
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    result = requests.get("https://api.github.com/repos/{}/{}/commits".
+                          format(owner, repo))
+
+    dict_json = result.json()
+    for i in dict_json:
+        print("{}: {}".format(i.get('sha'), i.get('commit').
+              get('author').get('name')))
