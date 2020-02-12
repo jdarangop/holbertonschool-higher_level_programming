2d12a6011346f3cd6eac87ee88901c93e1566817
jdarangop <juandiegoap33@gmail.com>
2020-02-12 03:50:35 +0000

    Fix: task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,9 +8,12 @@ request(process.argv[2], function (error, response, body) {
     console.log(error);
   } else {
     if (response.statusCode === 200) {
-      const result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0 };
+      const result = {};
       body = JSON.parse(body);
       for (let i = 0; i < body.length; i++) {
+        if (result[body[i].userId] === undefined) {
+          result[body[i].userId] = 0;
+        }
         if (body[i].completed === true) {
           const tmp = result[body[i].userId];
           result[body[i].userId] = tmp + 1;
