4aacb854b382aa813b9d9378a4be6c5dfe2abf41
jdarangop <juandiegoap33@gmail.com>
2020-02-12 04:42:51 +0000

    Fix: task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,7 +7,8 @@ request('http://swapi.co/api/films/' + process.argv[2], function (error, respons
   if (error) {
     console.log(error);
   } else {
-    const characters = JSON.parse(body).characters;
+    let characters = JSON.parse(body).characters;
+    characters = characters.sort(function (a, b) { return parseInt(a.split('/')[5], 10) - parseInt(b.split('/')[5], 10); });
     for (let i = 0; i < characters.length; i++) {
       request(characters[i], function (error, response, body) {
         if (error) {
